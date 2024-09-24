76aa806759066fef622e551cb7f54f11156ad8ad
liushanshan <liushanshan@jingrizf.com>
2024-09-24 20:56:19 +0800

    加入dubbo

--- src/main/java/com/bear/SpringBootSeateQiduoduoDemoApplication.java
+++ src/main/java/com/bear/SpringBootSeateQiduoduoDemoApplication.java
@@ -1,6 +1,7 @@
 package com.bear;
 
 import io.seata.spring.annotation.datasource.EnableAutoDataSourceProxy;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
